8d58c4c650131894605f69dd3e799d7b68acdd8d
daniil <bialetskidaniil@gmail.com>
2024-08-21 18:40:26 +0300

    pvz

--- callback.py
+++ callback.py
@@ -2,7 +2,7 @@ import sqlite3
 
 import strings
 from main import bot, types, inlineupdate
-from strings import get_money2
+from strings import get_money2, order_type_sel
 
 giveid = ""
 
@@ -20,23 +20,19 @@ def m(callback: types.CallbackQuery):
         connection.close()
 
     elif "order" in callback.data:
+        bot.delete_message(callback.message.chat.id, callback.message.message_id)
         cart = eval(callback.data.replace("order ", ""))
-        s = strings.order_list + callback.message.chat.first_name + ": \n"
-        for i in cart:
-            s += i + "\n"
         markup = types.InlineKeyboardMarkup()
-        btn1 = types.InlineKeyboardButton(strings.Button.accept, callback_data=f"yes {callback.message.chat.id}")
-        btn2 = types.InlineKeyboardButton(strings.Button.decline, callback_data=f"no {callback.message.chat.id}")
+        btn1 = types.InlineKeyboardButton(strings.Button.pvz, callback_data=f"pvz {cart}")
+        btn2 = types.InlineKeyboardButton(strings.Button.courier, callback_data=f"courier {cart}")
         markup.row(btn1, btn2)
-        bot.delete_message(callback.message.chat.id, callback.message.id)
-        bot.send_message(5645569833, s, reply_markup=markup)
+        bot.send_message(callback.message.chat.id, order_type_sel, reply_markup=markup)
 
-    elif "yes" in callback.data:
+    elif "yespvz" in callback.data:
         id = callback.data.split(" ")[1]
         bot.send_message(id, strings.accepted)
-        # bot.edit_message_text(strings.end, callback.message.chat.id, callback.message.id)
         markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton(strings.Button.done, callback_data=f"done {id}"))
+        markup.add(types.InlineKeyboardButton(strings.Button.done, callback_data=f"donepvz {id}"))
         bot.edit_message_reply_markup(callback.message.chat.id, callback.message.id, reply_markup=markup)
 
     elif "no" in callback.data:
@@ -44,16 +40,12 @@ def m(callback: types.CallbackQuery):
         bot.edit_message_reply_markup(callback.message.chat.id, callback.message.id, reply_markup=None)
         bot.send_message(callback.message.chat.id, strings.declined)
 
-    elif "done" in callback.data:
-        bot.send_message(callback.data.split(" ")[1], strings.done)
+    elif "donepvz" in callback.data:
+        bot.send_message(callback.data.split(" ")[1], strings.donepvz)
         bot.delete_message(callback.message.chat.id, callback.message.id)
         connection = sqlite3.connect("database.db")
         cursor = connection.cursor()
         cursor.execute(f'UPDATE users SET cart = "[]" WHERE id= "{callback.data.split(" ")[1]}"')
-        bot.send_message(callback.message.chat.id, strings.cart_clear)
-        connection.commit()
-        cursor.close()
-        connection.close()
         connection.commit()
         cursor.close()
         connection.close()
@@ -116,6 +108,59 @@ def m(callback: types.CallbackQuery):
         bot.register_next_step_handler(callback.message, give)
         bot.send_message(callback.message.chat.id, strings.enter_sum)
 
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+    elif "pvz" in callback.data:
+        cart = eval(callback.data.replace("pvz ", ""))
+        s = strings.order_list_pvz + callback.message.chat.first_name + ": \n"
+        for i in cart:
+            s += i + "\n"
+        markup = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton(strings.Button.accept, callback_data=f"yespvz {callback.message.chat.id}")
+        btn2 = types.InlineKeyboardButton(strings.Button.decline, callback_data=f"no {callback.message.chat.id}")
+        markup.row(btn1, btn2)
+        bot.delete_message(callback.message.chat.id, callback.message.id)
+        bot.send_message(5645569833, s, reply_markup=markup)
+
+    elif "courier" in callback.data:
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT sum FROM users WHERE id={callback.message.chat.id}")
+        sum = cursor.fetchone()[0]
+        sum = int(sum)
+        if sum < 15:
+            bot.send_message(callback.message.chat.id, strings.money_error)
+            return
+        cursor.execute(f"UPDATE users SET sum = {sum - 15} WHERE id={callback.message.chat.id}")
+        cart = eval(callback.data.replace("courier ", ""))
+        s = strings.order_list_courier + callback.message.chat.first_name + ": \n"
+        for i in cart:
+            s += i + "\n"
+        markup = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton(strings.Button.accept, callback_data=f"yesc {callback.message.chat.id}")
+        btn2 = types.InlineKeyboardButton(strings.Button.decline, callback_data=f"no {callback.message.chat.id}")
+        markup.row(btn1, btn2)
+        bot.delete_message(callback.message.chat.id, callback.message.id)
+        bot.send_message(5645569833, s, reply_markup=markup)
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+    elif "yesc" in callback.data:
+        id = callback.data.split(" ")[1]
+        bot.send_message(id, strings.accepted)
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton(strings.Button.done, callback_data=f"donec {id}"))
+        bot.edit_message_reply_markup(callback.message.chat.id, callback.message.id, reply_markup=markup)
+
+    elif "donec" in callback.data:
+        bot.send_message(callback.data.split(" ")[1], strings.donec)
+        bot.delete_message(callback.message.chat.id, callback.message.id)
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute(f'UPDATE users SET cart = "[]" WHERE id= "{callback.data.split(" ")[1]}"')
         connection.commit()
         cursor.close()
         connection.close()
